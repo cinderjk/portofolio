d4292fd0fb9ca61618ae99c04ec0848cb4d80ac8
cinderjk <cinderjk@gmail.com>
2026-03-15 04:38:00 +0700

    fix: update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://foe.my.id",
 	integrations: [mdx(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
